e9b2cefdb6481c761ab2ab71c7daa9b19f0759d4
jeremyreinhart <jeremyreinhart5@gmail.com>
2026-02-27 14:45:22 +0700

    update project

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -50,7 +50,7 @@ export const Projects = () => {
   ];
 
   return (
-    <section id="projects" className="max-w-6xl mx-auto px-4 py-10">
+    <section id="projects" className="max-w-6xl mx-auto px-4  scroll-mt-25">
       <div className="text-center mb-7">
         <h2 className="text-4xl font-bold tracking-tight">My Projects</h2>
 
